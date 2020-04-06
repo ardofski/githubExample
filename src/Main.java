d7e569a05a2f650ba7441cd98b2a1bd19154718f
Arda <ardagoktogan@gmail.com>
2020-04-06 23:07:45 +0300

    multiply implemented

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
                 int num2 = 10;
                 System.out.println( "NUmber 1 : " + num1);
                 System.out.println( "NUmber 2 : " + num2);
-                //System.out.println( "Mult : " + multipyl(num1,num2) );
+                System.out.println( "Mult : " + multipyl(num1,num2) );
                 //System.out.println( "Substraction : " + substraction(num1,num2) );
 
         }
