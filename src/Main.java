6671b2cf1654e20c2be1b80c5a3278b01609da88
Arda <ardagoktogan@gmail.com>
2020-04-06 21:33:22 +0300

    multiply method implemented.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
         public static void main(String []args){
+                int num1 = 5;
+                int num2 = 10;
+                System.out.println( "NUmber 1 : " + num1);
+                System.out.println( "NUmber 2 : " + num2);
 
+        }
 
+        public static int multipyl( int a, int b){
+                return a*b;
         }
 }
